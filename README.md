2fe890fa397713096a5c82ae381e07920ffdbb04
Kevin Eric <zhangzhp3@163.com>
2017-10-20 15:14:27 +0800

    例子--方法包装（代理）

---
+++ README.md
@@ -0,0 +1,17 @@
+
+
+git init
+Initialized empty Git repository in D:/gitproject/notebook/.git/
+git add README.md
+PS D:\gitproject\notebook> git commit -m "first commit"
+[master (root-commit) 261d7b2] first commit
+ 1 file changed, 0 insertions(+), 0 deletions(-)
+ create mode 100644 README.md
+git remote add origin https://github.com/kevinzzp/notebook.git
+git push -u origin master
+Counting objects: 3, done.
+Writing objects: 100% (3/3), 214 bytes | 0 bytes/s, done.
+Total 3 (delta 0), reused 0 (delta 0)
+To https://github.com/kevinzzp/notebook.git
+ * [new branch]      master -> master
+Branch master set up to track remote branch master from origin.
---
+++ wrap.js
@@ -0,0 +1,31 @@
+//系统基类
+function baseObject() {
+
+}
+baseObject.prototype.doSomething = function doSomething() {
+        console.log("doSomething after")
+    }
+    //包装方法
+function methodRewrite(doSomething) {
+    return function() {
+        var e = arguments;
+        console.log("i am " + e[0]);
+        return doSomething.call(this, e)
+    }
+}
+
+var myObject = baseObject.prototype;
+//调用包装方法
+methodWrap(myObject, "doSomething", methodRewrite)
+
+//包装方法
+function methodWrap(object, method, func) {
+    //获取prototype对应方法
+    var m = object[method]
+        //重新定义原始方法
+    object[method] = func(m)
+}
+
+//各处调用
+var m = new baseObject()
+m.doSomething("swimming");
